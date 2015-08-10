6d2ba9143ba65fc93a1a61d63043bbfbc9f54610
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-08-10 17:28:14 -0300

    Add all necessary routes for the application

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,24 @@
 'use strict'
 
 module.exports = function(app){
-  app.get('/', function(req, res, next){
-    res.status(200).json('Helllllloooooooo');
+  // create a dog
+  app.post('/dogs/new', function(req, res, next){
+    // res.status(200).json('Helllllloooooooo');
   });
+
+  // fetch all dogs
+  app.get('dogs', function(req, res, next){
+    // code here
+  });
+
+  // fetch individual dog by race
+  app.get('dogs/:race', function(req, res, next){
+    // code here
+  });
+
+  // create a new order
+  app.post('/order/new', function(req, res, next){
+    // code here
+  });
+
 };
