1d0f8e4d6e6eae011b8f0ec511206fd55cdcb858
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-08-11 19:38:54 -0300

    Fix Dogs collection

--- server/collections/dogs.js
+++ server/collections/dogs.js
@@ -1,7 +1,9 @@
 'use strict';
-
+var db = require('../db/config');
 var Dog = require('../models/dog');
 
-var Dogs = Bookshelf.Collection.extend({
+var Dogs = db.Collection.extend({
   model: Dog
 });
+
+module.exports = Dogs;
