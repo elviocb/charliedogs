a06902d1c726f17decee1465b457b9828457aec1
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-08-11 00:31:22 -0300

    Add new model and controller

---
+++ server/models/order.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var db = require('../db/config');
+var Dog = require('./dog');
+
+var Order = db.Model.extend({
+  tableName: 'orders',
+  hasTimestamps: true,
+  dogs: function() {
+    return this.hasMany(Dog);
+  }
+});
+
+module.exports = Order;
